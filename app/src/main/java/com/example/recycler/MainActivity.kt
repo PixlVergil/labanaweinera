93411607b438db91c4bdcbcd169cb4b62625ad05
SuprunovichKV <SuprunovichKV@DOM201.LOCAL>
2022-11-12 10:40:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recycler/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.recycler
+
+import RecyclerAdapter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val list = mutableListOf<String>()
+        for (i in 0..100) {
+            list.add(Math.random().toString())
+        }
+
+        val adapter = RecyclerAdapter(list)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+}
